86df5d2382017a0668c63cced27e07213c710655
Gary Wintle <gwintle@gmail.com>
2026-04-02 21:44:20 +0900

    fix formatTime hours

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,9 +1,9 @@
 function formatTime(time) {
   if (!time) return `0`;
 
-  const minutes = Math.floor(time / 60);
+  const minutes = Math.floor((time / 60) % 60);
   const seconds = time % 60;
-  const hours = Math.floor(minutes / 60);
+  const hours = Math.floor(time / 3600);
 
   const numPadding = (num) => {
     return String(num).padStart(2, 0);
