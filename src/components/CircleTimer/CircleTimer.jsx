a7a49ddb98f7f58d0ae8e000419af39fc92b042c
Gary Wintle <gwintle@gmail.com>
2026-04-13 00:11:29 +0900

    tried different timer colors

--- src/components/CircleTimer/CircleTimer.jsx
+++ src/components/CircleTimer/CircleTimer.jsx
@@ -4,51 +4,68 @@ const CircleTimer = ({ displayTime, duration }) => {
   const currentTime = Math.max(0, displayTime);
 
   const size = 350;
-  const strokeWidth = 8;
-  const backStrokeWidth = 12;
+  const strokeWidth = 6;
+  const backStrokeWidth = 6;
   const radius = size / 2 - strokeWidth;
   const circumference = 2 * Math.PI * radius;
   const offset = duration ? circumference * (currentTime / duration) : 0;
   const cx = size / 2;
   const cy = size / 2;
 
-  console.log({ currentTime, duration, offset, circumference });
-
   return (
     <div className={styles.container}>
       <svg viewBox="0 0 350 350">
         <defs>
-          <linearGradient id="timerGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-            <stop offset="0%" stopColor="#50E7C4" />
-            <stop offset="100%" stopColor="#2BCE8B" />
-          </linearGradient>
+          <radialGradient
+            id="timerGradient"
+            gradientUnits="userSpaceOnUse"
+            cx="0"
+            cy="175"
+            r="350"
+          >
+            <stop offset="10%" stopColor="#FF924C" />
+            <stop offset="100%" stopColor="#FFCA3A" />
+          </radialGradient>
+          <filter id="myBlur">
+            <feGaussianBlur stdDeviation="3" />
+          </filter>
         </defs>
-        {
-          <circle
-            className={styles.backCircle}
-            cx={cx}
-            cy={cy}
-            r={radius}
-            fill="none"
-            stroke="hsla(285, 7%, 22%, 0.2)"
-            strokeWidth={backStrokeWidth}
-          />
-        }
-        {
-          <circle
-            className={styles.progressCircle}
-            cx={cx}
-            cy={cy}
-            r={radius}
-            fill="none"
-            stroke="url(#timerGradient)"
-            strokeDasharray={circumference}
-            strokeDashoffset={offset}
-            strokeWidth={strokeWidth}
-            strokeLinecap="round"
-            transform={`rotate(-90 ${cx} ${cy})`}
-          />
-        }
+        <circle
+          className={styles.backCircle}
+          cx={cx}
+          cy={cy}
+          r={radius}
+          fill="none"
+          stroke="hsla(285, 10%, 10%, 0.1)"
+          strokeWidth={backStrokeWidth}
+        />
+        <circle
+          className={styles.progressCircle}
+          cx={cx}
+          cy={cy}
+          r={radius}
+          fill="none"
+          stroke="url(#timerGradient)"
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+          strokeWidth={strokeWidth}
+          strokeLinecap="round"
+          transform={`rotate(-90 ${cx} ${cy})`}
+          filter="url(#myBlur)"
+        />
+        <circle
+          className={styles.progressCircle}
+          cx={cx}
+          cy={cy}
+          r={radius}
+          fill="none"
+          stroke="url(#timerGradient)"
+          strokeDasharray={circumference}
+          strokeDashoffset={offset}
+          strokeWidth={strokeWidth}
+          strokeLinecap="round"
+          transform={`rotate(-90 ${cx} ${cy})`}
+        />
       </svg>
     </div>
   );
